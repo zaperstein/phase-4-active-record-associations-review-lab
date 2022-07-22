3a4bf125dd04b4ee624abb9434317639730d43e3
Maggie Saperstein <maggie.saperstein@gmail.com>
2022-07-22 14:11:36 -0500

    commit tree 2fca64f37c4a9e8ba9bb36de53db5e7ae87f8f82

--- db/migrate/20210506153127_create_rides.rb
+++ db/migrate/20210506153127_create_rides.rb
@@ -1,7 +1,8 @@
 class CreateRides < ActiveRecord::Migration[6.1]
   def change
     create_table :rides do |t|
-
+      t.integer :passenger_id
+      t.integer :taxi_id
       t.timestamps
     end
   end
